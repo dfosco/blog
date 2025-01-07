76b8d546b976ae5514fe37c5aa53612c36194e43
Daniel Fosco <dfosco@github.com>
2025-01-07 14:13:34 +0100

    Add pagefind WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    format: "file",
+  },  
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), pagefind()],
 });
